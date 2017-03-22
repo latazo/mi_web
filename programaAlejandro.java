78fa7f508afe3717f96b6ca2b339a1552f5b2bac
thor7a <thor7.a@gmail.com>
2017-03-22 14:31:30 +0100

    Merge origin/master
    Conflicts:
    	programaAlejandro.java

--- programaAlejandro.java
+++ programaAlejandro.java
@@ -5,7 +5,7 @@ public class programaAlejandro {
 
     public static void main(String[] args) {
    
-        System.out.println("El porcentaje de alumnos que no quieren que el Real Madrid se clasifique");
+        System.out.println("El porcentaje de alumnos que no quieren que el Real Madrid se clasifique son 42%");
         System.out.println("El porcentaje de alumnos que quieren que el barça gane la champions es del 42%");
     }
 }
