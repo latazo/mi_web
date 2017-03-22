740a389fd044b0b229fe108ca5a458a639413201
thor7a <thor7.a@gmail.com>
2017-03-22 13:56:56 +0100

    Primera subida

--- programaAlejandro.java
+++ programaAlejandro.java
@@ -5,5 +5,6 @@ public class programaAlejandro {
 
     public static void main(String[] args) {
         System.out.println("El porcentaje de alumnos que quieren que el barça gane la champions es del 40%");
+        System.out.println("El porcentaje de alumnos que no quieren que el Real Madrid se clasifique");
     }
 }
